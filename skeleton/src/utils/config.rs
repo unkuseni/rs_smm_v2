68a07573185c28f84d48e83594fdfeaa6d9c7357
unkuseni <amudalatadeoye@gmail.com>
2024-12-30 15:15:26 +0100

    added fn: watch_config

--- skeleton/src/utils/config.rs
+++ skeleton/src/utils/config.rs
@@ -15,12 +15,53 @@ where
     Ok(config)
 }
 
-pub async fn watch_config<T>(
+pub async fn watch_config<T, U>(
     path: T,
-    sender: mpsc::UnboundedSender<impl Clone>,
-) -> Result<(), notify::Error>
+    sender: mpsc::UnboundedSender<U>,
+) -> Result<(), Box<dyn Error>>
 where
     T: AsRef<Path> + Clone + Send + 'static,
+    U: serde::de::DeserializeOwned + Send + 'static,
 {
-  Ok(())
+    match read_toml::<T, U>(path.clone()) {
+        Ok(config) => {
+            sender.send(config)?;
+        }
+        Err(e) => {
+            println!("Error reading config file: {}", e);
+        }
+    }
+
+    // Channel for file system events
+    let (fs_tx, mut fs_rx) = tokio::sync::mpsc::channel(1);
+
+    // Create watcher
+    let mut watcher = RecommendedWatcher::new(
+        move |result: Result<Event, notify::Error>| {
+            if let Ok(event) = result {
+                if matches!(event.kind, EventKind::Modify(_)) {
+                    let _ = fs_tx.blocking_send(());
+                }
+            }
+        },
+        notify::Config::default(),
+    )?;
+
+    // Start watching the file
+    watcher.watch(path.as_ref(), RecursiveMode::NonRecursive)?;
+
+    while let Some(_) = fs_rx.recv().await {
+        match read_toml(path.clone()) {
+            Ok(new_config) => {
+                if let Err(e) = sender.send(new_config) {
+                    println!("Failed to send updated config: {}", e);
+                } else {
+                    println!("Config successfully updated");
+                }
+            }
+            Err(e) => println!("Failed to read updated config: {}", e),
+        }
+    }
+
+    Ok(())
 }
--- skeleton/tests/config.rs
+++ skeleton/tests/config.rs
@@ -1,11 +1,24 @@
 mod tests {
     use super::*;
-    use skeleton::utils::config::read_toml;
+    use skeleton::utils::config::{read_toml, watch_config};
     use skeleton::utils::models::Config;
     #[test]
     fn test_read_toml() {
         let config = read_toml::<&str, Config>("./tests/test.toml");
         println!("{:#?}", config);
+    }
 
+    #[tokio::test]
+    async fn test_watch_config() {
+        let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel::<Config>();
+
+        tokio::spawn(async move {
+            watch_config("./tests/test.toml", tx).await.unwrap();
+        });
+
+        while let Some(config) = rx.recv().await {
+            println!("{:#?}", config);
+        }
     }
 }
+deborah
