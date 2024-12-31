4a95320793aa1a3b4d2a609d317abadfaa5833d1
unkuseni <amudalatadeoye@gmail.com>
2024-12-31 01:38:12 +0100

    updated config

--- skeleton/src/utils/models.rs
+++ skeleton/src/utils/models.rs
@@ -2,16 +2,16 @@ use serde::Deserialize;
 
 #[derive(Deserialize, Debug, Clone)]
 pub struct Config {
-    pub logger_token: String,
-    pub chat_id: String,
+    pub token: String,
+    pub chat_id: i64,
 }
 
 impl PartialEq for Config {
     fn eq(&self, other: &Self) -> bool {
-        self.logger_token == other.logger_token && self.chat_id == other.chat_id
+        self.token == other.token && self.chat_id == other.chat_id
     }
 
     fn ne(&self, other: &Self) -> bool {
-        self.logger_token != other.logger_token && self.chat_id != other.chat_id
+        self.token != other.token && self.chat_id != other.chat_id
     }
 }
