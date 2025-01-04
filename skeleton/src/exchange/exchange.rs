849fa7bd71c40129a62adb30e538d921bd394c60
unkuseni <amudalatadeoye@gmail.com>
2025-01-04 16:59:23 +0100

    updated exchange trait

--- skeleton/src/exchange/exchange.rs
+++ skeleton/src/exchange/exchange.rs
@@ -10,8 +10,18 @@ pub trait Exchange {
     type FeeOutput;
     type LeverageOutput;
     type TraderOutput;
+    type StreamData;
     type StreamOutput;
+    type PrivateStreamData;
     type PrivateStreamOutput;
+    type PlaceOrderOutput;
+    type AmendOrderOutput;
+    type CancelOrderOutput;
+    type CancelAllOutput;
+    type BatchOrdersOutput;
+    type BatchAmendsOutput;
+    type SymbolInformationOutput;
+
     fn init(api_key: String, api_secret: String) -> Self;
     fn time(&self) -> impl Future<Output = Self::TimeOutput>;
     fn fees(&self, symbol: String) -> impl Future<Output = Self::FeeOutput>;
@@ -21,14 +31,43 @@ pub trait Exchange {
         leverage: u8,
     ) -> impl Future<Output = Self::LeverageOutput>;
     fn trader(&self, recv_window: u16) -> Self::TraderOutput;
+    fn place_order(
+        &self,
+        symbol: &str,
+        price: f64,
+        qty: f64,
+        is_buy: bool,
+    ) -> impl Future<Output = Self::PlaceOrderOutput>;
+    fn amend_order(
+        &self,
+        order_id: &str,
+        price: f64,
+        qty: f64,
+        symbol: &str,
+    ) -> impl Future<Output = Self::AmendOrderOutput>;
+    fn cancel_order(
+        &self,
+        order_id: &str,
+        symbol: &str,
+    ) -> impl Future<Output = Self::CancelOrderOutput>;
+    fn cancel_all(&self, symbol: &str) -> impl Future<Output = Self::CancelAllOutput>;
+    fn batch_orders(
+        &self,
+        orders: Vec<BatchOrder>,
+    ) -> impl Future<Output = Self::BatchOrdersOutput>;
+    fn batch_amends(
+        &self,
+        orders: Vec<BatchAmend>,
+    ) -> impl Future<Output = Self::BatchAmendsOutput>;
+    fn get_symbol_info(&self, symbol: &str) -> impl Future<Output = Self::SymbolInformationOutput>;
     fn market_subscribe(
         &self,
         symbols: Vec<String>,
-        sender: UnboundedSender<Self::StreamOutput>,
-    ) -> impl Future<Output = ()>;
+        sender: UnboundedSender<Self::StreamData>,
+    ) ->impl Future<Output = Self::StreamOutput>;
     fn private_subscribe(
         &self,
         symbol: String,
-        sender: UnboundedSender<Self::PrivateStreamOutput>,
-    ) -> impl Future<Output = ()>;
+        sender: UnboundedSender<Self::PrivateStreamData>,
+    ) -> impl Future<Output = Self::PrivateStreamOutput>;
 }
