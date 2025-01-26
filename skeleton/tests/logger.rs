d7f86520592fa34f32c521e32a61a4caa37f06c0
unkuseni <amudalatadeoye@gmail.com>
2025-01-26 18:54:30 +0100

    Refactor logger test to initialize Logger with LiveBot and improve test output

--- skeleton/tests/logger.rs
+++ skeleton/tests/logger.rs
@@ -4,10 +4,10 @@ mod tests {
 
     #[tokio::test]
     async fn test_logger() {
-        let bot = LiveBot::new().unwrap();
-        let log = Logger;
-        let _ = bot
-            .send_message(&log.info("This is a test, Please ignore"))
-            .await;
+        let bot = LiveBot::new("./tests/test.toml").await.unwrap();
+        let log = Logger::new(bot);
+        let _ = log.info("This is a test, Please ignore");
+        tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
+        println!("Logger test passed");
     }
 }
